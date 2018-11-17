a70b455cfe3da1d3d36578492f42fe04dfa58835
AZWebDDS <azwebdds@gmail.com>
2018-11-17 17:53:56 +0200

    Chore (modified files):Изменены файлы (n/a)
    ОБЫЧНОЕ НАПИСАНИЕ КОДА
    Модификация некоторых имеющихся файлов.
    В основном связано с настройкой BrowserSync в Gulp.
    
    package.json, gulpfile.js и др.
    Настройка Gulp BrowserSync не закончена.
    Изменено название каталога (название укорочено).
    e:\__MyDEV\www\2018\PHPMySQLRusakovVideokurs
    
    Фиксация перед переносом на Windows.
    (перенос через GitHub).
    
    Signed-off-by: AZWebDDS <azwebdds@gmail.com>

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,26 @@
 // gulpfile.js
 // Файл инструкций для работы Gulp (тасков)
 
-var gulp = require('gulp'),                         //Подключение Gulp из папки node_modules к проекту в переменную gulp
-    resizer = require('gulp-images-resizer'),       //Изменение размеров изображений
-    imagemin = require('gulp-imagemin'),            //Библиотека для работы с изображениями
-    pngquant = require('pngquant'),                 //Компрессор для PNG
-    cache = require('gulp-cache');                  //Кеширование изображений
+var gulp        = require('gulp'),                              //Подключение Gulp из папки node_modules к проекту в переменную gulp
+    browserSync = require('browser-sync'),                      //Подключаем Browser Sync
+    resizer     = require('gulp-images-resizer'),               //Изменение размеров изображений
+    imagemin    = require('gulp-imagemin'),                     //Библиотека для работы с изображениями
+    pngquant    = require('pngquant'),                          //Компрессор для PNG
+    cache       = require('gulp-cache');                        //Кеширование изображений
+
+
+//Таск для автоматического обновления браузера Browser Sync
+    gulp.task('browser-sync', function() {                      //Создаем таск browser-sync
+        browserSync({                                           //Выполняем browser Sync
+            server: {                                           //Определяем параметры сервера
+                baseDir: 'SANDBOX/public'                      //Директория для сервера
+            },
+            notify: true                                       //Отключаем уведомления
+        });
+    });
+
+
+
 
 
 // Таск resizer для изменения размеров изображений
@@ -43,4 +58,9 @@ gulp.task('mytask', function () {
     console.log('Привет, я таск!');
 });
 
+
+//Таск Наблюдатель
+gulp.task('watch', function() {
+    gulp.watch('SANDBOX/public/*.html', browserSync.reload);    //Наблюдение за HTML файлами в рабочем каталоге проекта
+});
 
